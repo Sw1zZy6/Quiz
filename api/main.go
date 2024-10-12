938d09c8cafba6762b438aa0e4013d34dbe48653
Kelvin <flamencycle2020@gmail>
2024-10-12 05:48:57 -0500

    added user signup/signin *backend*

---
+++ api/main.go
@@ -0,0 +1,174 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type user struct {	
+	Id string	`json:"id"`
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+var users = []user {
+	{Username: "Guest", Id: "1"},
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	users = []user{}
+
+	rows, err := db.Query("SELECT id, username FROM users")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var u user
+		if err := rows.Scan(&u.Id, &u.Username); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		users = append(users, u)
+	}
+	json.NewEncoder(w).Encode(users)
+}
+
+func getUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	var u user
+	err := db.QueryRow("SELECT id, username FROM users WHERE id = ?", params["id"]).Scan(&u.Id, &u.Username)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			http.NotFound(w, r)
+		} else {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
+	json.NewEncoder(w).Encode(u)
+}
+
+func submitUsername(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var u user
+	if err := json.NewDecoder(r.Body).Decode(&u); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if len(u.Password) < 1 {
+		http.Error(w, "Password cannot be empty.", http.StatusBadRequest)
+		return
+	}
+
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+
+
+	result, err := db.Exec("INSERT INTO users (username, password) VALUES (?, ?)", u.Username, hashedPassword)	
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	u.Id = strconv.Itoa(int(id))
+	u.Password = ""
+	json.NewEncoder(w).Encode(u)
+	fmt.Println("You've successfully added a new user: ", u.Username)
+}
+
+func updateUsername(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	var u user 
+	if err := json.NewDecoder(r.Body).Decode(&u); err != nil{
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	_, err := db.Exec("UPDATE users SET username = ? WHERE id = ?", u.Username, params["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	u.Id = params["id"]
+	json.NewEncoder(w).Encode(u)
+	fmt.Println("You've successfully updated user: ", u.Username)
+}	
+
+func deleteUser(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+
+	_, err := db.Exec("DELETE FROM users WHERE id = ?", params["id"]) 
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+	fmt.Println("Successfully deleted user with id: ", params["id"])
+}
+
+func verifyPassword(storedHash, password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(storedHash), []byte(password))
+	return err == nil
+}
+
+var db *sql.DB
+
+func main() {
+	var err error
+
+	dsn := "root:12345@tcp(127.0.0.1:3306)/quizdb"
+	db, err = sql.Open("mysql", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/users", submitUsername).Methods("POST")
+	r.HandleFunc("/users", getUsers).Methods("GET")
+	r.HandleFunc("/users/{id}", getUser).Methods("GET")
+	r.HandleFunc("/users/{id}", updateUsername).Methods("PUT")
+	r.HandleFunc("/users/{id}", deleteUser).Methods("DELETE")
+	
+	fmt.Println("Starting server on port 8000")
+	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(
+		handlers.AllowedOrigins([]string{"http://localhost:3000"}),
+		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "OPTIONS"}),
+		handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}),
+	)(r)))
+}
